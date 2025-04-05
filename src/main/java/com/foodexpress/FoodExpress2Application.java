931813c60a4e9893ff4d0c6456b69030982593f2
Neeraj <neerajsaraswat111@gmail.com>
2025-04-06 00:00:38 +0530

    make changes

--- src/main/java/com/foodexpress/FoodExpress2Application.java
+++ src/main/java/com/foodexpress/FoodExpress2Application.java
@@ -9,7 +9,15 @@ import io.github.cdimascio.dotenv.Dotenv;
 public class FoodExpress2Application {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load();
+		Dotenv dotenv = Dotenv.configure()
+				.directory("./FoodExpress") // path to your project root
+				.load();
+		System.setProperty("DB_URL", dotenv.get("DB_URL"));
+		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+		System.setProperty("MAIL_PORT", dotenv.get("MAIL_PORT"));
+		System.setProperty("MAIL_USERNAME", dotenv.get("MAIL_USERNAME"));
+		System.setProperty("MAIL_PASSWORD", dotenv.get("MAIL_PASSWORD"));
 		SpringApplication.run(FoodExpress2Application.class, args);
 	}
 
