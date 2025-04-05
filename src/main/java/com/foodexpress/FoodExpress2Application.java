3b6a76106cfc3ed4cf884c49998405ef4c4f3104
Neeraj <neerajsaraswat111@gmail.com>
2025-04-05 22:52:31 +0530

    added .env file

--- src/main/java/com/foodexpress/FoodExpress2Application.java
+++ src/main/java/com/foodexpress/FoodExpress2Application.java
@@ -3,10 +3,13 @@ package com.foodexpress;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class FoodExpress2Application {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
 		SpringApplication.run(FoodExpress2Application.class, args);
 	}
 
